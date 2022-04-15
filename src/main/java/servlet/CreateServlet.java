88a58f2f0797db95dd013b6618b5d0a57fda2667
sakagami <sakagamis0203@gmail.com>
2022-04-16 00:53:30 +0900

    スタイル見直し

--- src/main/java/servlet/CreateServlet.java
+++ src/main/java/servlet/CreateServlet.java
@@ -21,7 +21,7 @@ import model.CheckPostLogic;
 import model.NewLogic;
 
 @WebServlet("/create")
-@MultipartConfig(location="/upload", maxFileSize=5000000, maxRequestSize=5000000, fileSizeThreshold=5000000)
+@MultipartConfig(location="/tmp/files", maxFileSize=5000000, maxRequestSize=5000000, fileSizeThreshold=5000000)
 public class CreateServlet extends HttpServlet {
 
     private String image;
--- target/bookreviews-1.8-SNAPSHOT/WEB-INF/views/edit.jsp
+++ target/bookreviews-1.8-SNAPSHOT/WEB-INF/views/edit.jsp
@@ -1,8 +1,7 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
 <%@ page import="java.util.*" %>
- <% 
-
+<% 
   int showPostId = ((Integer)(request.getAttribute("postId"))).intValue();
   ArrayList<String> errorMessage = (ArrayList<String>)request.getAttribute("errorMessage");
   int intStars = ((Integer)(request.getAttribute("stars"))).intValue();
