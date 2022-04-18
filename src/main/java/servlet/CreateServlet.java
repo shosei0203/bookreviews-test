b300d8c8a24d4b400aae4122e19f4867da61662e
sakagami <sakagamis0203@gmail.com>
2022-04-19 00:08:18 +0900

    見直し

--- src/main/java/servlet/CreateServlet.java
+++ src/main/java/servlet/CreateServlet.java
@@ -16,6 +16,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import javax.servlet.*;
 
 import model.CheckPostLogic;
 import model.NewLogic;
--- src/main/java/servlet/UpdateServlet.java
+++ src/main/java/servlet/UpdateServlet.java
@@ -15,6 +15,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import javax.servlet.http.Part;
+import javax.servlet.*;
 
 import model.CheckPostLogic;
 import model.UpdateLogic;
