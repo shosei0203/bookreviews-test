af55f777801921d4fceed0a627c34bcb956255e8
sakagami <sakagamis0203@gmail.com>
2022-04-21 01:43:28 +0900

    見直し

--- src/main/java/servlet/UpdateServlet.java
+++ src/main/java/servlet/UpdateServlet.java
@@ -20,7 +20,7 @@ import model.CheckPostLogic;
 import model.UpdateLogic;
 
 @WebServlet("/update")
-@MultipartConfig(location="upload", maxFileSize=5000000, maxRequestSize=5000000, fileSizeThreshold=5000000)
+@MultipartConfig(location="/WebContent/upload", maxFileSize=5000000, maxRequestSize=5000000, fileSizeThreshold=5000000)
 public class UpdateServlet extends HttpServlet {
 
     private String image;
